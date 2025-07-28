87c20fcb3c13331c062ebfda81a794cc3facc7b4
马可 <78850005+makebl@users.noreply.github.com>
2025-07-28 18:06:16 +0800

    ·

--- action_schedule.sh
+++ action_schedule.sh
@@ -5,10 +5,10 @@ function set_env() { echo "$1=$2" >> $GITHUB_ENV; }
 
 APP_NAME="legado"
 APP_GIT_URL="https://github.com/gedoor/legado.git"
-APP_SUFFIX=""
+APP_SUFFIX="A"
 APP_WORKSPACE="/opt/$APP_NAME"
 APP_UPLOAD="$APP_WORKSPACE/app/build/outputs/apk/app/release"
-APP_LAUNCH_NAME="阅读"
+APP_LAUNCH_NAME="阅读.$APP_SUFFIX"
 
 GITHUB_API_LATEST="https://api.github.com/repos/gedoor/legado/releases/latest"
 
@@ -22,7 +22,7 @@ set_env SECRETS_MINIFY  $SECRETS_MINIFY
 set_env SECRETS_RENAME  $SECRETS_RENAME 
 
 LatestTag=$(curl -s $GITHUB_API_LATEST|jq .tag_name -r)
-LatestCheck=$(date -u -d"+8 hour" "+%Y-%m-%d--%S")
+LatestCheck=$(date -u -d"+8 hour" "+%Y-%m-%d %H:%M:%S")
 
 curl -s $GITHUB_API_LATEST|jq .body -r>/opt/latest.md
 
@@ -30,5 +30,5 @@ set_env LATEST_TAG        $LatestTag
 set_env APP_LATEST_TAG    $(echo $LatestTag|grep -o '3\.[0-9]\{2\}\.[0-9]\{6\}')
 set_env APP_LATEST_BODY   "/opt/latest.md"
 set_env APP_LATEST_CHECK  "$LatestCheck"
-set_env APP_UPLOAD_NAME   $APP_NAME-$LatestTag-$LatestCheck
+set_env APP_UPLOAD_NAME   $APP_NAME-$LatestTag
 set_env APP_LAST_TAG      $(cat $GITHUB_WORKSPACE/.lastcheck|sed -n 1p)
--- action_util.sh
+++ action_util.sh
@@ -6,7 +6,6 @@ function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" 
 function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
 function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
 
-
 function update_info()
 {
     cd $GITHUB_WORKSPACE
@@ -29,3 +28,11 @@ function update_info()
     fi
 }
 
+function webdav_upload()
+{
+    if [[ "$WEBDAV_UPLOAD" == "true" ]] && [[ -n "$WEBDAV_AUTH" ]] && [[ -n "$WEBDAV_URL" ]] && [[ "$REPO_OWNER" == "$REPO_ACTOR" ]]; then
+        debug "已开启webdav上传"
+        curl -X MKCOL -u $WEBDAV_AUTH $WEBDAV_URL
+        curl -u $WEBDAV_AUTH -T $1 $WEBDAV_URL
+    fi
+}
