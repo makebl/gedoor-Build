6b1707904432104da8e691b3a0d9905c3ba6a7cc
马可 <78850005+makebl@users.noreply.github.com>
2025-07-29 01:32:47 +0800

    Update action_clone.sh

--- action_clone.sh
+++ action_clone.sh
@@ -1,31 +1,24 @@
 #!/bin/sh
-#本脚本用来clone远端仓库
 source $GITHUB_WORKSPACE/action_util.sh
-#建立工作目录
+
 function init_workspace()
 {
-    git clone $APP_GIT_URL $APP_WORKSPACE
-    cd $APP_WORKSPACE
-    LatestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
-    if [[ -n "$SECRETS_TAG" ]] && [[ "$REPO_ACTOR" = "$REPO_OWNER" ]]; then
-        [[ "$SECRETS_TAG" = "master" ]] && LatestTag="master"
-        if [[ "$APP_NAME" = "legado" ]]; then 
-            if [[ -n "$(echo $SECRETS_TAG|grep -o '3\.[0-9]\{2\}\.[0-9]\{6\}')" ]]; then
-                LatestTag=$SECRETS_TAG
-            fi
-        elif [[ "$APP_NAME" = "MyBookshelf" ]]; then
-            if [[ -n "$(echo $SECRETS_TAG|grep -o '2\.[0-9]\{2\}\.[0-9]\{6\}')" ]]; then
-                LatestTag=$SECRETS_TAG
-            fi
-        else
-            [[ "$SECRETS_ENABLE" = "true" ]] && LatestTag=$SECRETS_TAG
-        fi
-    fi
-    git checkout $LatestTag
-    set_env APP_UPLOAD_NAME $APP_NAME-$LatestTag
-    [[ "$APP_NAME" = "legado" ]] && \
-    set_env APP_TAG    $(echo $LatestTag|grep -o '3\.[0-9]\{2\}\.[0-9]\{6\}')
-    debug "$APP_NAME latest tag is $LatestTag"
-}
-init_workspace;
+    debug "克隆 master 分支源码"
 
+    if [ -z "$APP_GIT_URL" ]; then
+        echo "❌ APP_GIT_URL 为空，无法克隆仓库"
+        exit 1
+    fi
+
+    git clone --branch master "$APP_GIT_URL" "$APP_WORKSPACE"
+    if [ ! -d "$APP_WORKSPACE" ]; then
+        echo "❌ 克隆失败，$APP_WORKSPACE 目录不存在"
+        exit 1
+    fi
+
+    cd "$APP_WORKSPACE"
+
+    set_env APP_UPLOAD_NAME "$APP_NAME-master-$(date +'%Y%m%d-%H%M')"
+}
+
+init_workspace
